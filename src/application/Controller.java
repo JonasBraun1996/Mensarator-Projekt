e326e2b07463b4cbf13788f2becd9d46fd7794ba
Jonas Braun <braunjonas1996googlemail.com>
2019-02-13 15:36:14 +0100

    Initial commit

---
+++ src/application/Controller.java
@@ -0,0 +1,51 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+
+public class Controller {
+
+    @FXML
+    private Tab hauptspeiseTab;
+
+    @FXML
+    private Button startseite;
+
+    @FXML
+    private Button hauptspeiseWeiter;
+
+    @FXML
+    private Tab beilageTab;
+
+    @FXML
+    private Button beilageWeiter;
+
+    @FXML
+    private Button beilageZurueck;
+
+    @FXML
+    private Tab getraenkeTab;
+
+    @FXML
+    private Button getraenkeWeiter;
+
+    @FXML
+    private Button getraenkeZurueck;
+
+    @FXML
+    private Tab warenkorbTab;
+
+    @FXML
+    private Button bezahlen;
+
+    @FXML
+    private Button warenkorbZurueck;
+
+    @FXML
+    void hauptspeiseWeiterClicked(ActionEvent event) {
+
+    }
+
+}
