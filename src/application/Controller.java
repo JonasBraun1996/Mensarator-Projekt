7e3fb2defcebd9e7acf8d9c7bb7faadd10555a57
Aline666 <aline.schaub@web.de>
2019-02-15 09:30:27 +0100

    Button Navigation ohne Start und Endseite
    hab die Navigation der Tabs geändert aber da wir noch keine Start und endseite ham  hab ich die mal weggelassen

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -3,13 +3,19 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
-
+	
     @FXML
     private Tab hauptspeiseTab;
 
+    @FXML
+    private TabPane tabPane;
+    
     @FXML
     private Button startseite;
 
@@ -46,6 +52,43 @@ public class Controller {
     @FXML
     void hauptspeiseWeiterClicked(ActionEvent event) {
 
+    	tabPane.getSelectionModel().select(beilageTab);   	    	
+    	
+    }
+    
+    @FXML
+    void beilageZurueckClicked(ActionEvent event) {
+
+    	tabPane.getSelectionModel().select(hauptspeiseTab);   	    	
+    	
+    }
+    
+    
+    @FXML
+    void beilageWeiterClicked(ActionEvent event) {
+
+    	tabPane.getSelectionModel().select(getraenkeTab);   	    	
+    	
+    }
+    
+    @FXML
+    void GetraenkeZurueckClicked(ActionEvent event) {
+
+    	tabPane.getSelectionModel().select(beilageTab);   	    	
+    	
+    }
+    @FXML
+    void GetraenkeWeiterClicked(ActionEvent event) {
+
+    	tabPane.getSelectionModel().select(warenkorbTab);   	    	
+    	
+    }
+    
+    @FXML
+    void WarenkorbZurueckClicked(ActionEvent event) {
+
+    	tabPane.getSelectionModel().select(getraenkeTab);   	    	
+    	
     }
 
 }
