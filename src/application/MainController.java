342f850b01b9d1ab0427b4c7b0cf71e910c36644
Jonas Braun <braunjonas1996googlemail.com>
2019-03-24 22:59:22 +0100

    Startseite von dem Konfigurator getrennt

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -5,7 +5,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import hauptspeise.Controllerhauptspeise;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,59 +15,40 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class MainController extends Application implements Initializable {
-	
+public class MainController implements Initializable {
+
 	@FXML
-    private BorderPane root;
-	
+	private BorderPane root;
+
 	@FXML
 	private Button startConfiguration;
-	
-	@FXML
-	private Button startMensarator;
-	
-	boolean initialize = false;
-	
-	@FXML
-	void startClicked (ActionEvent event) {
-		initialize = true;
-	}
-	
+
 	public static Controllerhauptspeise controllerHauptspeise;
 	
+	Stage secondStage = new Stage();
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-		if(initialize){
-			try {
-	
-				FXMLLoader loaderHauptspeise = new FXMLLoader(getClass().getResource("../hauptspeise/hauptspeisepane.fxml"));
-				BorderPane centerPane = loaderHauptspeise.load();
-				root.setCenter(centerPane);
-				controllerHauptspeise = loaderHauptspeise.getController();
-				Parent view = centerPane;
-				Scene scene = new Scene(view);
-				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-				
-	
-	
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		if(initialize) {
-			FXMLLoader loaderHauptspeise = new FXMLLoader(getClass().getResource("../hauptspeise/hauptspeisepane.fxml"));
-			BorderPane centerPane = loaderHauptspeise.load();
-			Parent view = centerPane;
-			Scene scene = new Scene(view);
-			primaryStage.setTitle("Mensarator");
-			primaryStage.setScene(scene);
-			primaryStage.show();
+		FXMLLoader loaderHauptspeise = new FXMLLoader(getClass().getResource("Test.fxml"));
+		Parent view = null;
+		try {
+		view = loaderHauptspeise.load();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
+		Scene scene = new Scene(view);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		secondStage.setTitle("Konfigurator");
+		secondStage.setScene(scene);
+		
+		
+		
 	}
+    @FXML
+    void start(ActionEvent event) {
+		secondStage.show();
+    }
 }
---
+++ src/application/TestController.java
@@ -0,0 +1,188 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
+
+public class TestController {
+	
+    @FXML
+    private TextArea SummefeldHS;
+
+    @FXML
+    private Tab hauptspeiseTab;
+
+    @FXML
+    private Button startseite;
+
+    @FXML
+    private Button hauptspeiseWeiter;
+
+    @FXML
+    private RadioButton Menue1;
+
+    @FXML
+    private ToggleGroup menue;
+
+    @FXML
+    private RadioButton Menue2;
+
+    @FXML
+    private RadioButton Menue3;
+
+    @FXML
+    private TextArea SummefeldHS1;
+
+    @FXML
+    private Tab beilageTab;
+
+    @FXML
+    private Button beilageWeiter;
+
+    @FXML
+    private Button beilageZurueck;
+
+    @FXML
+    private TextArea SummenfeldBL;
+
+    @FXML
+    private RadioButton Pommes;
+
+    @FXML
+    private RadioButton Reis;
+
+    @FXML
+    private RadioButton Spaetzle;
+
+    @FXML
+    private RadioButton Kroketten;
+
+    @FXML
+    private RadioButton Suppe;
+
+    @FXML
+    private RadioButton Nudeln;
+
+    @FXML
+    private RadioButton Gemuese;
+
+    @FXML
+    private RadioButton Salat;
+
+    @FXML
+    private RadioButton Kartoffelbrei;
+
+    @FXML
+    private RadioButton Popcorn;
+
+    @FXML
+    private RadioButton Rahmsosse;
+
+    @FXML
+    private RadioButton Jaegersosse;
+
+    @FXML
+    private RadioButton Bratensosse;
+
+    @FXML
+    private Tab getraenkeTab;
+
+    @FXML
+    private Button getraenkeWeiter;
+
+    @FXML
+    private Button getraenkeZurueck;
+
+    @FXML
+    private RadioButton Cola;
+
+    @FXML
+    private RadioButton Fanta;
+
+    @FXML
+    private RadioButton Apfelsaft;
+
+    @FXML
+    private RadioButton Sprite;
+
+    @FXML
+    private RadioButton Wasser;
+
+    @FXML
+    private RadioButton Spezi;
+
+    @FXML
+    private RadioButton CapriSonne;
+
+    @FXML
+    private RadioButton RedBull;
+
+    @FXML
+    private RadioButton Ayran;
+
+    @FXML
+    private TextArea SummenfeldGetraenk;
+
+    @FXML
+    private Tab warenkorbTab;
+
+    @FXML
+    private Button bezahlenBtn;
+
+    @FXML
+    private Button warenkorbZurueck;
+    
+    @FXML
+    void GetraenkeWeiterClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GetraenkeZurueckClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PreisberechnenBL(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PreisberechnenHS(ActionEvent event) {
+
+    }
+
+    @FXML
+    void WarenkorbZurueckClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ZurueckZurStartseite(ActionEvent event) {
+
+    }
+
+    @FXML
+    void beilageWeiterClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void beilageZurueckClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bezahlenOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hauptspeiseWeiterClicked(ActionEvent event) {
+
+    }
+}
